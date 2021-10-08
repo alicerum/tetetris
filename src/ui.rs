2b0c6461ce6eb974b40b766f54793f96b246f94f
Alice Rum <alice.rum@redhat.com>
2021-10-08 22:57:10 +0200

    Figuring out the layout thing it seems
    Managed to do something with the layout, so far so good, it almost
    works, next thing i'll need to split it into the small grid, and after
    that hopefully will be able to fiddle with all the different size
    settings.

--- src/ui.rs
+++ src/ui.rs
@@ -1,37 +1,54 @@
 use tui::backend::Backend;
 use tui::Frame;
+use tui::text::Span;
+use tui::style::{Style, Color, Modifier};
 use tui::layout::{Layout, Direction, Constraint};
-use tui::widgets::{Block, Borders};
+use tui::widgets::{Block, Borders, BorderType};
 
 pub fn draw<B: Backend>(f: &mut Frame<B>) {
-    let chunks = Layout::default()
+
+    let cell_height = 2;
+    let cell_width = 4;
+
+    let board_height = 20 * cell_height + 2;
+    let board_width = 10 * cell_width + 2;
+
+    let term_rect = f.size();
+
+    let b = Block::default()
+        .title(
+            Span::styled("TeTetris",
+                Style::default().fg(Color::Red).add_modifier(Modifier::BOLD))
+        )
+        .borders(Borders::ALL)
+        .border_type(BorderType::Rounded);
+
+    f.render_widget(b, term_rect);
+
+    let vpadding = (term_rect.height - board_height) / 2;
+    let hpadding = (term_rect.width - board_width) / 2;
+
+    let outer = Layout::default()
         .direction(Direction::Vertical)
         .margin(1)
-        .constraints(
-            [
-                Constraint::Percentage(10),
-                Constraint::Percentage(80),
-                Constraint::Percentage(10)
-            ].as_ref()
-        )
-        .split(f.size());
+        .constraints(vec![
+            Constraint::Min(vpadding),
+            Constraint::Length(board_height),
+            Constraint::Min(vpadding),
+        ])
+        .split(term_rect);
 
-    let chunks = Layout::default()
+    let outer = Layout::default()
         .direction(Direction::Horizontal)
-        .margin(10)
-        .constraints(
-            [
-                Constraint::Min(22),
-                Constraint::Min(20),
-            ].as_ref()
-        )
-        .split(f.size());
+        .margin(1)
+        .constraints(vec![
+            Constraint::Min(hpadding),
+            Constraint::Length(board_width),
+            Constraint::Min(hpadding),
+        ])
+        .split(outer[1]);
+
     let block = Block::default()
-         .title("Tetris")
-         .borders(Borders::ALL);
-    f.render_widget(block, chunks[0]);
-    let block = Block::default()
-         .title("Block 2")
-         .borders(Borders::ALL);
-    f.render_widget(block, chunks[1]);
+        .borders(Borders::ALL);
+    f.render_widget(block, outer[1]);
 }
