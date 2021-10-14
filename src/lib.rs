acd65bd398da796c0a325fffde14522b617b9ba8
Alice Rum <alice.rum@redhat.com>
2021-10-14 21:28:51 +0200

    Added alternate screen for output

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ use std::error::Error;
 use tui::Terminal;
 use tui::backend::TermionBackend;
 use termion::raw::IntoRawMode;
+use termion::screen::AlternateScreen;
 use termion::event::Key;
 use events::{Event, Events};
 use game::Board;
@@ -14,6 +15,7 @@ pub mod flags;
 
 pub fn run(f: flags::Flags) -> Result<(), Box<dyn Error>> {
     let stdout = io::stdout().into_raw_mode()?;
+    let stdout = AlternateScreen::from(stdout);
     let backend = TermionBackend::new(stdout);
 
     let mut terminal = Terminal::new(backend)?;
